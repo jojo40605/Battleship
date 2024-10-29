f83dbd2475203fdeead47670aa9fc5dc6979b939
jojo40605 <63128770+jojo40605@users.noreply.github.com>
2024-10-29 16:41:49 -0600

    Delete lab01 directory

--- lab01/components/mono/pac0.h
+++
@@ -1,9 +0,0 @@
-
-#include <stdint.h>
-
-#define PAC0_BITS_PER_PIXEL 1
-#define PAC0_LENGTH 128
-#define PAC0_W 32
-#define PAC0_H 32
-
-extern const uint8_t pac0[PAC0_LENGTH];
--- lab01/components/mono/pac1.h
+++
@@ -1,9 +0,0 @@
-
-#include <stdint.h>
-
-#define PAC1_BITS_PER_PIXEL 1
-#define PAC1_LENGTH 128
-#define PAC1_W 32
-#define PAC1_H 32
-
-extern const uint8_t pac1[PAC1_LENGTH];
