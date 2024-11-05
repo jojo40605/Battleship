5a86c64ac0c46fb197a2fa01dddd1676a25e627d
Joseph Nelson <jcn00@DIGITAL-44.ee.byu.edu>
2024-11-04 18:04:24 -0700

    finished the ship placement

--- lab07/main/game.c
+++ lab07/main/game.c
@@ -17,10 +17,11 @@
 
 #define BIT_SHIFT 4
 #define FULL_LOW_NIB 0x0F
-#define MAX_SHIPS 4
+#define MAX_SHIPS 5
 #define delayMS(ms) \
 	vTaskDelay(((ms)+(portTICK_PERIOD_MS-1))/portTICK_PERIOD_MS)
 
+#define RGB565(r, g, b)  (((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3))
 
 
 // States for the SM
@@ -35,19 +36,25 @@ enum Battleship_st_t {
 };
 static enum Battleship_st_t currentState;
 
-// Structure for a ship
-typedef struct {
-    int length;       // Length of the ship
-    int r;            // Row position
-    int c;            // Column position
-    bool horizontal;  // Orientation of the ship
-    bool sunk;        // If the ship is sunk
-    color_t color;    //Color of placement
-} ship_t;
+
+//Enum for ship colors
+const uint16_t ship_colors[MAX_SHIPS] = {
+    RGB565(0, 255, 0),   // Bright green
+    RGB565(255, 165, 0), // Bright orange
+    RGB565(160, 32, 240), // Bright purple
+    RGB565(0, 150, 0),    // Medium green
+    RGB565(255, 100, 0),  // Darker orange
+    RGB565(75, 0, 130),   // Indigo
+    RGB565(34, 139, 34),  // Forest green
+    RGB565(255, 69, 0),   // Red-orange
+    RGB565(218, 112, 214), // Orchid
+    RGB565(173, 255, 47), // Green-yellow
+    RGB565(238, 130, 238) // Violet
+};
 
 /*TODOS FOR JOSEPH
     create enum for colors and set unique ship color in new_game_st
-    fix bug where when placing ship the last cursor is not removed
+    fix bug where when placing ship the last cursor is not removed -- erase now always erases one extra
     create array to actually save the ship placement
 
     create a helper function to see if ship placement is valid
@@ -58,6 +65,8 @@ typedef struct {
 static ship_t ships[MAX_SHIPS];
 static int ship_count;
 static int hits;
+static int myShips[CONFIG_BOARD_R][CONFIG_BOARD_C];
+static int enemyShips[CONFIG_BOARD_R][CONFIG_BOARD_C];
 //if total hits for a single player == total sizes of all ships then game over
 
 // Initialize SM
@@ -125,14 +134,14 @@ void game_tick() {
             // SET NAV TO CENTER
             nav_set_loc(0,0);
             // DISPLAY NEXT PLAYER
-            graphics_drawMessage("Next Player: X", WHITE, CONFIG_BACK_CLR);
+            graphics_drawMessage("PLACE YOUR SHIPS (PLACE-A ROTATE-B)", WHITE, CONFIG_BACK_CLR);
             lcd_writeFrame();
 
             //create ships size 1 - MAX_SHIPS
             for(int i = 0; i < MAX_SHIPS; i++){
                 ships[i].length = i+1;
                 ships[i].horizontal = true;
-                ships[i].color = CYAN;
+                ships[i].color = ship_colors[i % (sizeof(ship_colors) / sizeof(ship_colors[0]))];
             }
 
             //MILESTONE 2
@@ -142,25 +151,25 @@ void game_tick() {
             }
             break;
         case ship_place_st:
-                bool shipPlaced = false;
                 lcd_frameEnable();
                 nav_get_loc(&r, &c);
                 
                 if ((r != prev_r) || (c != prev_c) || (prev_horizontal != currHor)){ //if the cursor moved or rotated
-                graphics_drawHighlight(r,c,WHITE);
+                graphics_drawHighlight(r,c,WHITE); //draw cursor
+                graphics_drawMessage("PLACE YOUR SHIPS (PLACE-A ROTATE-B)", WHITE, CONFIG_BACK_CLR); //redraw text
 
-                    //erase previous cursor
+                    //erase previous higlights
                     if (prev_horizontal){ // was Horizontal
-                        for (int i = 0; i < ships[numShip].length; i++){
+                        for (int i = 0; i < ships[numShip].length + 1; i++){
                             graphics_drawHighlight(prev_r, prev_c + i, CONFIG_BACK_CLR);
                         }
                     } else{
-                        for (int i = 0; i < ships[numShip].length; i++){
+                        for (int i = 0; i < ships[numShip].length + 1; i++){
                             graphics_drawHighlight(prev_r + i, prev_c, CONFIG_BACK_CLR);
                         }
                     }
 
-                    //draw new cursor
+                    //draw new higlights
                     if (currHor){ //Horizontal
                         for (int i = 0; i < ships[numShip].length; i++){
                             graphics_drawHighlight(r,c+i,WHITE);
@@ -185,29 +194,36 @@ void game_tick() {
                 
                 if (!pin_get_level(HW_BTN_B)) { //rotate
                     currHor = !currHor;
+                    for(;;){
+                            if (pin_get_level(HW_BTN_B)) break;
+                        };
                 }
 
-                if (!pin_get_level(HW_BTN_A)) { //place
-                    graphics_drawHighlight(r,c,CYAN);
-                    if (currHor){ //Horizontal
-                        for (int i = 0; i < ships[numShip].length; i++){
-                            graphics_drawShip(r,c+i,ships[numShip].color);
+                if (!pin_get_level(HW_BTN_A)) { //Place ship button pressed
+                    if (is_validShip(ships[numShip], r, c, currHor)){ //ship place is valid
+                        graphics_drawHighlight(r,c,CYAN);
+                        myShips[r][c] = 1;
+                        if (currHor){ //Horizontal
+                            for (int i = 0; i < ships[numShip].length; i++){
+                                graphics_drawShip(r,c+i,ships[numShip].color);
+                                myShips[r][c+i] = 1;
+                            }
+                        } else{
+                            for (int i = 0; i < ships[numShip].length; i++){
+                                graphics_drawShip(r+i,c,ships[numShip].color);
+                                myShips[r+i][c] = 1;
+                            }
                         }
-                    } else{
-                        for (int i = 0; i < ships[numShip].length; i++){
-                            graphics_drawShip(r+i,c,ships[numShip].color);
-                        }
-                    }
-                    if (!shipPlaced){
                         numShip--; //move to next ship     
-                        shipPlaced = true;
                         for(;;){
                             if (pin_get_level(HW_BTN_A)) break;
                         };
+                    }else{ //can't place
+                        //TODO ADD VISUAL ERROR NOTICE
                     }                     
                 }
                 
-                printf("%d\n", numShip); 
+                
                 break;
         case attack_wait_st:
             
@@ -240,6 +256,29 @@ void game_tick() {
             printf("ERROR ACTION");
             break;
     }
+}
+
+bool is_validShip(ship_t shipToCheck, int8_t r, int8_t c, bool currHor){
+    //check cursor
+    if (myShips[r][c] !=0){
+        return false;
+    }
+
+    //check for other ships or edges
+    if (currHor){ //Horizontal
+        for (int i = 0; i < shipToCheck.length; i++){
+            if ((myShips[r][c+i] != 0) || (c+i > CONFIG_BOARD_C)){
+                return false;
+            };
+        }
+    } else{ //Vertical
+        for (int i = 0; i < shipToCheck.length; i++){
+            if ((myShips[r+i][c] != 0) || (r+i > CONFIG_BOARD_R)){
+                return false;
+            };
+        }
+    }
+    return true; //no issues detected
 }
 
 bool is_hit(int8_t row, int8_t column) {
--- lab07/main/game.h
+++ lab07/main/game.h
@@ -1,6 +1,16 @@
 #ifndef GAME_H_
 #define GAME_H_
 
+// Structure for a ship
+typedef struct {
+    int length;       // Length of the ship
+    int r;            // Row position
+    int c;            // Column position
+    bool horizontal;  // Orientation of the ship
+    bool sunk;        // If the ship is sunk
+    color_t color;    //Color of placement
+} ship_t;
+
 // Initialize the game logic.
 void game_init(void);
 
@@ -8,5 +18,7 @@ void game_init(void);
 void game_tick(void);
 
 bool is_hit(int8_t r, int8_t c);
+
+bool is_validShip(ship_t shipToCheck, int8_t r, int8_t c, bool currHor);
 
 #endif // GAME_H_
